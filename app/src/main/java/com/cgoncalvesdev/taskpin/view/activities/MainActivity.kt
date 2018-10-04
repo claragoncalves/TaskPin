934f9db698fd9d8fe33e9f385157bd95ce215cc4
Unknown <claragoncalves.dev@gmail.com>
2018-10-04 14:57:16 -0300

    some design

--- app/src/main/java/com/cgoncalvesdev/taskpin/view/activities/MainActivity.kt
+++ app/src/main/java/com/cgoncalvesdev/taskpin/view/activities/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import com.cgoncalvesdev.taskpin.R
 import com.cgoncalvesdev.taskpin.model.pojo.Task
 import com.cgoncalvesdev.taskpin.util.NotificationReceiver
 import com.cgoncalvesdev.taskpin.view.fragments.FragmentMainTaskList
 import com.cgoncalvesdev.taskpin.view.fragments.FragmentTaskDetail
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity(), FragmentMainTaskList.TaskTapListener, FragmentMainTaskList.TaskNotificationSetter {
 
@@ -24,11 +28,32 @@ class MainActivity : AppCompatActivity(), FragmentMainTaskList.TaskTapListener, 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         placeFragment(fragmentMainTaskList, FRAGMENT_MAIN_TASK_LIST)
-        button_add_task.setOnClickListener {
-            val intent = Intent(this, NewTaskActivity::class.java)
+        setupToolbar()
+    }
+
+    fun setupToolbar(){
+        val toolbar: Toolbar = findViewById(R.id.toolbar_include)
+        toolbar.title = resources.getString(R.string.app_name)
+        //        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setNavigationIcon(R.drawable.ic_pin)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId?.equals(R.id.add_action)!!){
+            var intent = Intent(this, NewTaskActivity::class.java)
             startActivity(intent)
+            return true
+        }else {
+            return super.onOptionsItemSelected(item)
         }
     }
+
 
     fun placeFragment(fragment: Fragment, tag: String){
         val fragmentManager = supportFragmentManager
